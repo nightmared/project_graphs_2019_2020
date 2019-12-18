b3b0a43ccd52ccc7779eb7e9be75b073f80cff58
Simon THOBY <git@nightmared.fr>
2019-12-18 17:13:25 +0100

    update zach

--- Graph_Project/src/girvan.ml
+++ Graph_Project/src/girvan.ml
@@ -1,4 +1,10 @@
+open Zach.Zach
 
-let betweenness_score g = ();;
+let betweenness_score g =
+    let edges_in_shortest_paths = fold_vertex (fun v acc ->
+        (fold_vertex (fun v_dist acc -> if v != v_dist then (fst (shortest_path g v v_dist))@acc else acc) g [])@acc
+        ) g []
+    in let sorted_edges_in_shortest_paths = List.sort (fun a b -> if a = b then 0 else -1) edges_in_shortest_paths
+    in ();;
 
 let girvan_newman g n_com = ();;
