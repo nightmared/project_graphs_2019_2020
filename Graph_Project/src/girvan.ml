d3b998d9e1fe259f7ba607780f66e86957fe7fe9
Simon THOBY <git@nightmared.fr>
2020-01-03 18:29:37 +0100

    ajout de af.ml pour visualiser les graphes

--- Graph_Project/src/girvan.ml
+++ Graph_Project/src/girvan.ml
@@ -1,15 +1,25 @@
 open Zach.Zach
 
+let is_same_edge e1 e2 =
+	(E.dst e1 = E.dst e2 && E.src e1 = E.src e2)
+	|| (E.src e2 = E.dst e1 && E.dst e2 = E.src e1)
+
+let compare_edge e1 e2 =
+	if is_same_edge e1 e2 then 0 else compare e1 e2
+
 let betweenness_score g =
-    let edges_in_shortest_paths = fold_vertex (fun v acc ->
-        (fold_vertex (fun v_dist acc -> if v != v_dist then (fst (shortest_path g v v_dist))@acc else acc) g [])@acc
-        ) g []
-    in let sorted_edges_in_shortest_paths = List.sort (fun a b -> if a = b then 0 else -1) edges_in_shortest_paths
-    in let edges_with_numbers_list = List.fold_left (fun acc e -> match acc with
-        | (new_e, i)::q when new_e = e -> (e, i+1)::q
-        | _ -> (e, 1)::acc
-        ) [] sorted_edges_in_shortest_paths
-    in edges_with_numbers_list;;
+	(* pour tous les noeuds, énumère le chemin le plus court vers les autres noeuds *)
+	let edges_in_shortest_paths = fold_vertex (fun v acc ->
+		(fold_vertex (fun v_dist acc -> if v != v_dist then (fst (shortest_path g v v_dist))@acc else acc) g [])@acc
+		) g []
+	(* trie cette liste par arrête *)
+	in let sorted_edges_in_shortest_paths = List.sort compare edges_in_shortest_paths
+	(* compte le nombre de fois où chaque arrête est présente *)
+	in let edges_with_numbers_list = List.fold_left (fun acc e -> match acc with
+		| (new_e, i)::q when is_same_edge e new_e -> (e, i+1)::q
+		| _ -> (e, 1)::acc
+		) [] sorted_edges_in_shortest_paths
+	in edges_with_numbers_list;;
 
 let girvan_newman g n_com = 
-    let b_score = betweenness_score g in ();;
+	let b_score = betweenness_score g in ();;
